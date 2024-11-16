53e8cfb83bc9f8e1003fc278c29bfdd7f4e5d513
nestorcastelblanco <nestorfelipe10@gmail.com>
2024-11-15 22:03:09 -0500

    .

--- src/main/java/co/edu/uniquindio/UniEventos/config/FirebaseConfig.java
+++ src/main/java/co/edu/uniquindio/UniEventos/config/FirebaseConfig.java
@@ -19,8 +19,8 @@ public class  FirebaseConfig {
 
     @Bean
     public FirebaseApp intializeFirebase() throws IOException {
-        
-        InputStream serviceAccount = new ClassPathResource("src/main/resources/uniquindio-36aa1-firebase-adminsdk-5vup3-bbd2742635.json").getInputStream();
+
+        InputStream serviceAccount = new ClassPathResource("unieventos-aca74-firebase-adminsdk-zp03a-d4f2d64f50.json").getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
